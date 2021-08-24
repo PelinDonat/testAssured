2504103a37ca2578acb4dd11abcbfa45aad0c40a
pelin <pelin.donat.3919@gmail.com>
2021-08-24 20:29:38 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class ZippoTest {
+
+    @Test
+    public void test()
+    {
+       given().
+               //hazirlik islemleri
+
+                       when().
+               //link ve aksiyon islemleri
+
+                       then() ;
+               // test ve extract islemleri
+
+    }
+}
